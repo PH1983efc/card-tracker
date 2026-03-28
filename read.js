9a48771ff6fb839bf63efbe69c514fdbe0c97db2
PH1983efc <phealyuk@gmail.com>
2026-03-28 16:16:58 +0000

    Add files via upload

---
+++ read.js
@@ -0,0 +1,18 @@
+import { google } from 'googleapis';
+
+export default async function handler(req, res) {
+  const auth = new google.auth.JWT(
+    process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    null,
+    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  );
+
+  const sheets = google.sheets({ version: 'v4', auth });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: 'Sheet1', // change if your sheet tab has a different name
+  });
+
+  res.json({ success: true, rows: response.data.values });
+}
